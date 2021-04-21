2adc07118cce29fdba0263e25b2a8a3018b4c673
Ryan <rcastriotta1@gmail.com>
2021-04-21 14:59:56 -0400

    Refactored Code

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,31 @@
 const express = require('express')
 const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 const http = require('http');
-var bodyParser = require('body-parser');
-
 
 const WebSocket = require('ws');
 const plivo = require('plivo');
 
-const client = new plivo.Client("MAYZA3N2QZYZU3MTY1NT", "YjIwOWI1NTVmOTMwY2M3NjVkMDA3ZWI4NzZjY2U4");
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server })
 const { v4: uuidv4 } = require('uuid');
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(function(req, response, next) {
-    response.contentType('application/xml');
+const baseURL = 'http://01b28c6e0874.ngrok.io';
+const client = new plivo.Client(
+    process.env.PILVO_AUTH_ID,
+    process.env.PILVO_AUTH_TOKEN
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.contentType('application/xml');
     next();
 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', (ws) => {
     ws.id = uuidv4();
 
     ws.on('message', (msg) => {
@@ -31,12 +33,12 @@ wss.on('connection', function connection(ws) {
         client.calls.create(
                 `+1${sendFrom}`, // from
                 `+1${sendTo}`, // to
-                `http://01b28c6e0874.ngrok.io/record?url=${audio}&id=${ws.id}`, // answer url
+                `${baseURL}/record?url=${audio}&id=${ws.id}`, // answer url
                 {
                     answerMethod: "GET",
                     machineDetection: true, // Used to detect if the call has been answered by a machine. The valid values are true and hangup.
                     machineDetectionTime: "5000", // ime allotted to analyze if the call has been answered by a machine. The default value is 5000 ms.
-                    machineDetectionUrl: `http://01b28c6e0874.ngrok.io/detect?id=${ws.id}`, // A URL where machine detection parameters will be sent by Plivo.
+                    machineDetectionUrl: `${baseURL}/detect?id=${ws.id}`, // A URL where machine detection parameters will be sent by Plivo.
                     machineDetectionMethod: "GET" // The method used to call the machineDetectionUrl
                 },
             )
@@ -45,19 +47,20 @@ wss.on('connection', function connection(ws) {
     })
 })
 
-app.all('/record', (request, resp) => {
-    const { url, id } = request.query;
+app.all('/record', (req, res) => {
+    const { url, id } = req.query;
 
     wss.clients.forEach(client => {
         if (client.id === id) {
             client.send('Chatting...')
         }
     })
+
     var record_params = {
         'recordSession': 'true',
         'redirect': 'false',
         'method': "GET", // HTTP method to submit the action URL
-        'callbackUrl': `http://01b28c6e0874.ngrok.io/record_callback?id=${id}`, // If set, this URL is fired in background when the recorded file is ready to be used.
+        'callbackUrl': `${baseURL}/record_callback?id=${id}`, // If set, this URL is fired in background when the recorded file is ready to be used.
         'callbackMethod': "GET" // Method used to notify the callbackUrl.
     }
 
@@ -67,12 +70,12 @@ app.all('/record', (request, resp) => {
 
     r.addPlay(url);
 
-    resp.setHeader("Content-Type", "text/xml");
-    resp.end(r.toXML());
+    res.setHeader("Content-Type", "text/xml");
+    res.end(r.toXML());
 })
 
-app.all('/record_callback', (request, response) => {
-    const { id, RecordUrl, RecordingDuration } = request.query;
+app.all('/record_callback', (req, res) => {
+    const { id, RecordUrl, RecordingDuration } = req.query;
 
     wss.clients.forEach(client => {
         if (client.id === id) {
@@ -85,7 +88,7 @@ app.all('/record_callback', (request, response) => {
         client.terminate();
     })
 
-    response.status(200).end()
+    res.status(200).end()
 });
 
 app.all('/detect', async(req, res) => {
@@ -99,4 +102,5 @@ app.all('/sms_callback', async(req, res) => {
     res.status(200).end();
 })
 
-server.listen(3001, () => console.log(`Listening on port :3000`))
+const PORT = process.env.PORT || 80;
+server.listen(PORT, () => console.log(`Listening on port :${PORT}`))
